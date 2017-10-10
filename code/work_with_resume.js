c36366f402abd5bafaeb35a342b5de5e4363c3b9
sergio <srgmatveev@gmail.com>
2017-10-10 12:45:51 +0300

    first commit

---
+++ code/work_with_resume.js
@@ -0,0 +1,8 @@
+let https = require('https');
+
+function resume_update_function(token)
+{
+    console.log(token);
+}
+
+exports.resume_update = resume_update_function;
---
+++ index.js
@@ -0,0 +1,6 @@
+const access_token  =
+'QHN9T9CTTQ065T8HCMIUGINCG5EM0GB92SAURD37RO30AA00FBLEAKFHIFL2D5LI';
+
+var resume = require('./code/work_with_resume');
+
+resume.resume_update(access_token);
