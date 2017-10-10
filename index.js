3644558a9a07d7da48515e905656ed04d897f965
sergio <srgmatveev@gmail.com>
2017-10-10 17:02:37 +0300

    8 commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const access_token  =
-'QHN9T9CTTQ065T8HCMIUGINCG5EM0GB92SAURD37RO30AA00FBLEAKFHIFL2D5LI';
+'QHN9*************************************************L2D5LI';
 
 var resume = require('./code/work_with_resume');
 
